7cf2dc66d3f531e7e13187534df9859321e9b4fa
MorivVV <morivvv@gmail.com>
2021-10-26 23:10:02 +0400

    Delete public directory

--- public/ajax/configs/localhost.php
+++
@@ -1,7 +0,0 @@
-<?php
-// ассоциативный массив
-return array('host' => "127.0.0.1"
-    , 'user' => "root"
-    , 'password' => ""
-    , 'db_name' => "b32_18009385_users"
-    , 'charset' => "utf8");
